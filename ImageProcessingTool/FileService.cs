6c39bed1fdb57ba9190b03feb1f5a9a64f6fde19
Andrea Noventa <andrea.noventa@outlook.com>
2025-03-27 23:38:59 +0100

    Rimosso metodo LoadFileInfo da FileService

--- ImageProcessingTool/FileService.cs
+++ ImageProcessingTool/FileService.cs
@@ -7,27 +7,6 @@ public sealed class FileService
     private const string FilePath = $"{Path}/{FileName}";
 
 
-    public static List<FileInfo> LoadFileInfo(
-        string? path,
-        string searchPattern)
-    {
-        if (!Directory.Exists(path))
-        {
-            throw new Exception("La directory selezionata non esiste.");
-        }
-
-        var fileList = new List<FileInfo>();
-
-        foreach (var fileName in Directory.GetFiles(path, searchPattern))
-        {
-            var fileInfo = new FileInfo(fileName);
-
-            fileList.Add(fileInfo);
-        }
-
-        return fileList;
-    }
-
     public static void WriteToCsv(
         ProcessedImage processedImage)
     {
