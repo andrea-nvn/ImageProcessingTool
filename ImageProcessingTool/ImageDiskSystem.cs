319f5e09b587e9c273bfecbac3ab01431ed28449
Andrea Noventa <andrea.noventa@outlook.com>
2025-03-27 23:36:49 +0100

    Creata classe ImageDiskSystem

---
+++ ImageProcessingTool/ImageDiskSystem.cs
@@ -0,0 +1,32 @@
+﻿using ImageProcessingTool.VisionSystem;
+
+namespace ImageProcessingTool;
+
+public sealed class ImageDiskSystem : IVisionSystem
+{
+    public List<FileInfo> AcquireImages()
+    {
+        Console.WriteLine("Inserisci il percorso dei file da analizzare: ");
+        var path = Console.ReadLine();
+
+        if (!Directory.Exists(path))
+        {
+            throw new Exception("La directory selezionata non esiste.");
+        }
+
+        var pngImages = Directory.GetFiles(path, "*.png");
+        var jpgImages = Directory.GetFiles(path, "*.jpg");
+        var images = pngImages.Concat(jpgImages);
+
+        var imagesFileInfo = new List<FileInfo>();
+
+        foreach (var image in images)
+        {
+            var fileInfo = new FileInfo(image);
+
+            imagesFileInfo.Add(fileInfo);
+        }
+
+        return imagesFileInfo;
+    }
+}
