6c605885283bb6b105e05d676173993574cf2838
Andrea Noventa <andrea.noventa@outlook.com>
2025-03-28 03:00:03 +0100

    Creato AnalysisTxtFileHandler

---
+++ ImageProcessingTool/AnalysisResults/AnalysisTxtFileHandler.cs
@@ -0,0 +1,64 @@
+﻿using ImageUtilities;
+using ImageUtilities.Interfaces;
+
+namespace ImageProcessingTool.AnalysisResults;
+
+public sealed class AnalysisTxtFileHandler : IAnalysisFileHandler
+{
+    private const string Path = "./results";
+    private const string FileName = "AnalysisResults.txt";
+    private const string FilePath = $"{Path}/{FileName}";
+
+
+    public void SaveAnalysis(
+        ImageAnalysisResults processedImage)
+    {
+        if (!Directory.Exists(Path))
+        {
+            Directory.CreateDirectory(Path);
+        }
+
+        using var writer = new StreamWriter(FilePath, append: true);
+        if (new FileInfo(FilePath).Length == 0)
+        {
+            writer.WriteLine("NomeFile,Dimensione,LuminositàStimata,DataAnalisi");
+        }
+
+        writer.WriteLine($"{processedImage.NomeFile},{processedImage.Dimensione},{processedImage.LuminositàStimata},{processedImage.DataAnalisi}");
+    }
+
+    public void SearchByBrightnessThreshold(
+        double threshold)
+    {
+        if (!File.Exists(FilePath))
+        {
+            throw new Exception("Non è stata trovata nessuna analisi precedente.");
+        }
+
+        using var sr = new StreamReader(FilePath);
+        bool found = false;
+
+        sr.ReadLine();
+
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+
+            var fields = line?.Split(',') ?? [];
+
+            if (double.TryParse(fields[2], out double value))
+            {
+                if (value > threshold)
+                {
+                    Console.WriteLine($"{line}");
+                    found = true;
+                }
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine($"Non è stato trovato nessun valore superiore a {threshold}.");
+        }
+    }
+}
